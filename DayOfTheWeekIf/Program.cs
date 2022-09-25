bdc0521c7e24bf67dc859c4f2eee9040fd86f6bd
Erko Targamaa <erkotargamaa@gmail.com>
2022-09-25 17:01:37 +0300

    week3

---
+++ DayOfTheWeekIf/Program.cs
@@ -0,0 +1,35 @@
+﻿
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+Console.WriteLine(weekDay);
+
+if(weekDay == 0)
+{
+    Console.WriteLine("Pühapäev");
+}
+else if (weekDay == 1)
+{
+    Console.WriteLine("Esmaspäev");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("Teisipäev");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("Kolmapäev");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("Neljapäev");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("Reedepäev");
+}
+else
+{
+    Console.WriteLine("Laupäev");  
+}
+
+Console.WriteLine("Ilusat päeva!");
---
+++ OddOrEven/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Sisesta number: ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//paaris või paaritu
+
+int result = userNum % 2;
+
+if(result != 0)
+{
+    Console.WriteLine("Number on paarituarv");
+}
+else
+{
+    Console.WriteLine("Number on paarisarv");
+}
+
